66ef080186b24878ce3dadd97caa0430e6f16904
Sivaganesh B <133204043+siva-2006@users.noreply.github.com>
2025-08-14 18:01:10 +0530

    Delete assets/tflite_audio directory

--- assets/tflite_audio/android/src/main/java/flutter/tflite_audio/AudioChunk.java
+++
@@ -1,7 +0,0 @@
-package flutter.tflite_audio;
-
-
-// https://stackoverflow.com/questions/54566753/escaping-closure-in-swift-and-how-to-perform-it-in-java
-public interface AudioChunk {
-    void get(short [] data);
-}
